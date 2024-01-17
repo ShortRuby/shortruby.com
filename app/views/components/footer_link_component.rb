ab4fafebd2b41c0250bc2ca6bdd326109d9a38d6
Lucian Ghinda <lucianghinda@users.noreply.github.com>
2024-01-17 09:56:44 +0200

    Add Terms of use to the main website (#12)

---
+++ app/views/components/footer_link_component.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class FooterLinkComponent < ApplicationComponent
+  include Phlex::Rails::Helpers::LinkTo
+
+  CLASS = <<~DEFAULT
+    inline-flex items-center justify-center
+    rounded-md font-medium transition-colors
+    focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring
+    disabled:pointer-events-none disabled:opacity-50
+    text-gray-600 underline-offset-4 hover:underline
+    h-9 uppercase
+  DEFAULT
+
+  def initialize(url:, text: nil, size: "lg", **options)
+    @text = text
+    @url = url
+    @options = options
+    @size = size
+  end
+
+  def template(&)
+    if @text.present?
+      link_to(@text, @url, class: "#{CLASS} #{text_size}")
+    else
+      link_to(@url, class: "#{CLASS} #{text_size}", &)
+    end
+  end
+
+  private
+
+  def text_size = "text-#{@size}"
+end
